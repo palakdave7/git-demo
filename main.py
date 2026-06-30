93f62cd7250008ea6682e74f0115a5a24a9bfc83
Palak Dave <palakdave2023@gmail.com>
2026-06-30 15:44:21 +0530

    added path parameter for redability in docs formatting

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path
 import json
 app=FastAPI()
 
@@ -24,7 +24,7 @@ def view():
     return data
 
 @app.get('/patient/{patient_id}')
-def view_patient(patient_id:str):
+def view_patient(patient_id:str=Path(...,description='id of patient',example='P001')):
     #load data
     data=load_data()
     
