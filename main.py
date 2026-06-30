bfd9d17463614bfc16fd1c7d7cecc6e0bbca8ca4
Palak Dave <palakdave2023@gmail.com>
2026-06-30 15:41:33 +0530

    simple fecth operation specfic id

--- main.py
+++ main.py
@@ -22,3 +22,13 @@ def view():
     data=load_data()
 
     return data
+
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id:str):
+    #load data
+    data=load_data()
+    
+    if patient_id in data:
+        return data[patient_id]
+    
+    return {'error':'not found'}
