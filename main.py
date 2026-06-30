55bc25b477ccd0c09b4e85d69cef922d8e1afbd5
Palak Dave <palakdave2023@gmail.com>
2026-06-30 15:31:01 +0530

    simple fecth operation

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
+import json
 app=FastAPI()
 
+
+def load_data():
+    with open('patients.json','r') as f:
+        data=json.load(f)
+
+    return data
+    
 @app.get("/")
 def hello():
-    return {'message':'hello ji'}
+    return {'message':'Patient Management System API'}
 
 @app.get('/2')
 def hello2():
-    return {'message':'hello ji2'}
+    return {'message':'A Fully Functional System API to manage your patient records'}
+
+@app.get('/view')
+def view():
+    data=load_data()
+
+    return data
